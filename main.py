839d7a880b9f18433e8f0e780c5ca782b8691c38
Technomag <edwin.raczko@student.uw.edu.pl>
2015-11-30 13:01:17 +0100

    Test importowania z foldera
    Testowanie dzialania dwoch funkcji. Dodanie linii zawierajacej
    informacje o kdowaniu znakow

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#import test_lib
+import os
+import sys
+#---------------------------------------------
+#Dodanie foldera lib do sciezki python
+#Umozliwia importowanie modulow z tego foldera
+sys.path.append(os.getcwd()+"/lib")
+#---------------------------------------------
+
+import test_lib
+
+
+if __name__ == "__main__":
+    print test_lib.dodaj(3,2)
+    print test_lib.znajdz_literke("Geografia","a")
