31b9927e2e393995c1137d29590fd10609bbad8e
TechnmagStub <edwinraczko@gmail.com>
2015-12-06 22:13:45 +0100

    Dodano 2 funkcje do modulu mathlosowe

---
+++ lib/math_losowe.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 06 22:10:58 2015
+
+@author: Edwin
+"""
+
+import random
+
+def RandomSum(dlugosc_sekwencji):
+    """Zwraca sumę z sekwencji dowolnej długości. Elementy
+       sekwencji są losowane <0-1)
+    """
+    sekwencja = []
+    for i in range(dlugosc_sekwencji):
+        sekwencja.append(random.random())
+    return sum(sekwencja)
+
+def RandomSumRange(dlugosc_sekwencji,koncowa_wartosc):
+    """Zwraca sumę z sekwencji dowolnej długości. Elementy
+       sekwencji są integerami losowanymi z zakresu
+       0 - koncowa_wartosc    
+    """
+    sekwencja =[]
+    for i in range(dlugosc_sekwencji):
+        sekwencja.append(random.randint(0,koncowa_wartosc))
+    return sum(sekwencja)
+    
+if __name__ == "__main__":
+    print RandomSum(10)
+    print RandomSumRange(10,10)
