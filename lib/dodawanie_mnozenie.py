0469b1b23e386badeba1b3291d414a8806de1ed2
szczesnakasia <szczesnakasia@gmail.com>
2015-12-08 10:26:18 +0100

    dodanie funkcji dodawanie i mnozenie

---
+++ lib/dodawanie_mnozenie.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 08 10:11:36 2015
+
+@author: student
+"""
+
+def dodawanie(x,y):
+    suma=x+y
+    return suma
+         
+def mnozenie(x,y):
+    iloczyn=x*y
+    return iloczyn
+        
