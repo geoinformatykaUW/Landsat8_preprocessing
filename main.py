a0ad9558b7b6b313643f61ccb470399f79e3ac1e
Technomag <edwin.raczko@student.uw.edu.pl>
2015-12-08 18:48:15 +0100

    sprzatanie po zajeciach

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-
+import sys,os
 #---------------------------------------------
 #Dodanie foldera lib do sciezki python
 #Umozliwia importowanie modulow z tego foldera
 sys.path.append(os.getcwd()+"/lib")
 #---------------------------------------------
 
-import test_lib
-
 if __name__ == "__main__":
-    print test_lib.dodaj(3,2)
-    print test_lib.znajdz_literke("Geografia","a")
-	
-#Trzeba cos dodac
-
-#Tu tez
+    pass
