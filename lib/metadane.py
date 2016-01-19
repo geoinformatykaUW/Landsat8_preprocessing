5353beff8794fb8ca21b22e38cacac3e9a207090
ARynk <alicja.e.rynkiewicz@gmail.com>
2016-01-19 11:16:39 +0100

    Zmodyfikowano metadane.py

--- lib/metadane.py
+++ lib/metadane.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
 #Wyciągnięcie danych z pliku z metadanymi w postaci obiektu i jego atrybutów
+import os
 
-def wczytanie_danych(plik):
-	"""
-	Wczytywanie danych z pliku z metadanymi satelity Landsat 8.
-	"""
-	p = open(plik, 'r')
-	pojemnik = {}
-	for linia in p:
-		elementy = linia.split("=")
-		if len(elementy) == 2:
-			wartosc = elementy[1].strip().strip('"')
-			try:
-				wartosc = float(wartosc)
-			except:
-				pass
-			pojemnik[elementy[0].strip()] = wartosc
-	p.close()
-	return pojemnik
 	
 class Struct(object):
 	"""
@@ -30,23 +14,46 @@ class Dane(object):
 	"""
 	Klasa posiadająca atrybuty zgodne z metadanymi
 	"""
-	def __init__(self,slownik):
-		self.dane = slownik
-		#self.data = slownik['DATE_ACQUIRED']
+	def __init__(self):
+		pass
+		
 	def czyt_atrybuty(self):
 		"""
 		Metoda wczytuje metadane ze słownika
 		"""
-		obj = Struct(**self.dane)
+		obj = Struct(**self.pojemnik)
+		del self.pojemnik
 		self.metadane = obj
+		
+	def wczytanie_danych(self,plik):
+		"""
+		Wczytywanie danych z pliku z metadanymi satelity Landsat 8.
+		"""
+		p = open(plik, 'r')
+		pojemnik = {}
+		for linia in p:
+			elementy = linia.split("=")
+			if len(elementy) == 2:
+				wartosc = elementy[1].strip().strip('"')
+				try:
+					wartosc = float(wartosc)
+				except:
+					pass
+				pojemnik[elementy[0].strip()] = wartosc
+		p.close()
+		self.pojemnik = pojemnik
+	
 		
 
 	
 if __name__=="__main__":
 	try:
 		print os.getcwd()
-		dane_slownik = wczytanie_danych("..\dane\sceny\LC81910232015182LGN00_MTL.txt")
-		metadane = Dane(dane_slownik)
+		scena_landsat = Dane()
+		scena_landsat.wczytanie_danych("..\dane\sceny\LC81910232015182LGN00_MTL.txt")
+		#metadane = Dane(dane_slownik)
+		
+		scena_landsat.czyt_atrybuty()
 		
 	except IOError as e:
 		print "Nie ma takiego pliku. Sprawdz nazwe lub sciezke"
--- main.py
+++ main.py
@@ -6,10 +6,18 @@ import sys, os
 sys.path.append(os.getcwd()+"/lib")
 #---------------------------------------------
 import unpacking
+import metadane
 
 if __name__ == "__main__":
-    
-    in_file="dane/LC81910232015182LGN00.tar.gz"
-    write_dir="dane/sceny"
+	
+	in_file="dane/LC81910232015182LGN00.tar.gz"
+	write_dir="dane/sceny"
+	#unpacking.un_gzip_file(in_file, write_dir)   
 
-    unpacking.un_gzip_file(in_file, write_dir)   
+
+	
+	
+	scena_landsat = metadane.Dane()
+	scena_landsat.wczytanie_danych("dane\sceny\LC81910232015182LGN00_MTL.txt")
+	scena_landsat.czyt_atrybuty()
+	prawdziwe_metadane = scena_landsat.metadane
